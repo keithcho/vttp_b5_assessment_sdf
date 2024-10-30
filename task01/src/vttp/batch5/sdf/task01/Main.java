002259295c5fd171d4e8388283e5a70b4440ffd8
Keith <135153337+keithcho@users.noreply.github.com>
2024-10-30 11:27:54 +0800

    complete task 1

--- task01/src/vttp/batch5/sdf/task01/Main.java
+++ task01/src/vttp/batch5/sdf/task01/Main.java
@@ -1,12 +1,50 @@
 package vttp.batch5.sdf.task01;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Collections;
+import java.util.Map;
+import java.util.TreeMap;
+import vttp.batch5.sdf.task01.models.BikeEntry;
+
 // Use this class as the entry point of your program
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static Map<Integer, BikeEntry> readCSV(Reader reader) throws IOException {
+		Map<Integer, BikeEntry> dataMap = new TreeMap<>();
+		BufferedReader br = new BufferedReader(reader);
+		String line;
 
-		System.out.printf("hello, world\n");
+		// Allocate first line to headers
+        String[] cellHeader = br.readLine().split(",");
 
+		// Read .csv file using BufferedReader
+		while ((line = br.readLine()) != null) {
+            String[] cellRow = line.split(",");
+			Integer totalCyclists = Integer.parseInt(cellRow[8]) + Integer.parseInt(cellRow[9]);
+			dataMap.put(totalCyclists, BikeEntry.toBikeEntry(cellRow));
+        }
+		return dataMap;
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		String csvPath = "day.csv";
+		Reader reader = new FileReader(csvPath);
+
+		Map<Integer, BikeEntry> dataMap = readCSV(reader);
+
+		// Sort data into descending order
+		Map<Integer, BikeEntry> dataMapDescending = new TreeMap<>(Collections.reverseOrder());
+		dataMapDescending.putAll(dataMap);
+
+		// Print top 5 days with the most cyclists
+		Object[] outputArr = dataMapDescending.keySet().toArray();
+		for (int i = 0; i < 5; i++) {
+			OutputHelper.printOutput(dataMapDescending.get(outputArr[i]), i);
+		}
 	}
 }
---
+++ task01/src/vttp/batch5/sdf/task01/OutputHelper.java
@@ -0,0 +1,55 @@
+package vttp.batch5.sdf.task01;
+
+import vttp.batch5.sdf.task01.models.BikeEntry;
+
+// Helper class to print output
+
+public class OutputHelper {
+
+    public static final String[] POSITION = { "highest", "second highest", "third highest", "fourth highest", "fifth highest" };
+    public static final String[] WEATHER = { "Clear, Few clouds, Partly cloudy, Partly cloudy",
+    "Mist + Cloudy, Mist + Broken clouds, Mist + Few clouds, Mist",
+    "Light Snow, Light Rain + Thunderstorm + Scattered clouds, Light Rain + Scattered clouds",
+    "Heavy Rain + Ice Pallets + Thunderstorm + Mist, Snow + Fog" };
+
+    public static String toPosition(int position) {
+		switch (position) {
+			case 0:
+			case 1:
+			case 2:
+			case 3:
+			default:
+				return "not in top 5";
+		}
+	}
+
+	public static String toWeather(int weather) {
+		switch (weather) {
+			case 1:
+			case 2:
+			case 3:
+			case 4:
+				return WEATHER[weather - 1];
+			default:
+				return "unreported";
+		}
+	}
+
+	public static String isHoliday(boolean holiday) {
+		if (holiday) {
+			return "a holiday";
+		} else {
+			return "not a holiday";
+		}
+	}
+
+    public static void printOutput(BikeEntry rowData, int index) {
+		int cyclistCount = rowData.getCasual() + rowData.getRegistered();
+		System.out.println("The " + POSITION[index] + " recorded number of cyclists was in "
+		+ Utilities.toSeason(rowData.getSeason()) + ", on a " + Utilities.toWeekday(rowData.getWeekday())
+		+ " in the month of " + Utilities.toMonth(rowData.getMonth()) + ". There were a total of "
+		+ cyclistCount + " cyclists. The weather was " + WEATHER[rowData.getWeather()-1] + ". "
+		+ Utilities.toWeekday(rowData.getWeekday()) + " was " + isHoliday(rowData.isHoliday()) + ".");
+		System.out.println();
+	}
+}
